e658ad4c14174cf539dda018db2bebe6b3de914b
health4u01 <health4u01@gmail.com>
2015-08-28 01:43:24 -0700

    including hashSSHA function which was mistakenly deleted

--- site/www/public_html/android_api/include/DB_Functions.php
+++ site/www/public_html/android_api/include/DB_Functions.php
@@ -67,4 +67,18 @@ class DB_Functions {
             return false;
         }
     }
+    
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
 }
