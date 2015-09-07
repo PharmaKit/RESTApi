e72ec210c52c84050d50d43b49ca32379cd12086
health4u01 <health4u01@gmail.com>
2015-09-07 04:20:18 -0700

    introduced slim framework and also introduced ability to upload user feedback.

--- site/www/public_html/android_api/reports/reportupload.php
+++ site/www/public_html/android_api/reports/reportupload.php
@@ -3,7 +3,7 @@
     $fileName = date('Y-m-d_H-i-s').'.txt';
     $file = fopen($fileName,'w') or die('Could not create report file: ' . $fileName);
     foreach($_POST as $key => $value) {
-    $reportLine = $key." = ".$value."\n";
+        $reportLine = $key." = ".$value."\n";
         fwrite($file, $reportLine) or die ('Could not write to report file ' . $reportLine);
     }
     fclose($file);
